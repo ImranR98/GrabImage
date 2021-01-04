b46c1e1a187c6605a708eae84771e0810ce47d44
Imran Remtulla <iremtulla15@gmail.com>
2021-01-04 15:12:28 +0300

    Break out of loop when done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const loop = async () => {
     while (true) { // Keep trying until manually aborted
         try {
             await funcs.grabAndSaveSeveralImages(queries, config.outputImagesDir, config.minutesDelay, config.msWait, config.maxImageIndex, config.skipExisting, config.randomizeDelay, false)
+            break;
         } catch (err) {
             console.error('Error: ' + err)
         }
